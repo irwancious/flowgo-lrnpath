8dc33261ef634538eb0d9db4837142b774a538a7
irwancious <80471722+irwancious@users.noreply.github.com>
2022-10-03 09:21:28 +0700

    Add files via upload

---
+++ GANJIL GENAP-JAVA/irwan.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bilangan.genap;
+import java.util.*;
+import java.lang.Math;
+/**
+ *
+ * @author acer
+ */
+public class irwan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int angka;
+        
+        angka = 0;
+        while (angka < 10) {
+            angka = angka + 2;
+            System.out.println("bilangan ke- " + angka);
+        }
+    }
+    
+}
